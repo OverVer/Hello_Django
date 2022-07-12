dd127c11b9f4b2a20547fb8485f2e8dc6d72242a
Lenovo <key_02_tvs@mail.ru>
2022-07-12 13:44:05 +0300

    start_files

---
+++ hello_django/hello_django/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def about(request):
+    a = 5+6
+    return render (request, 'about.html', {'gritting': a})
+
+def home(request):
+    return HttpResponse ('This is my home')
